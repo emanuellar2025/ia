2b4a1edda5ee7c01b22d76dab8738f5ed1277594
emanuellar2025 <r.emanuella@escola.pr.gov.br>
2025-09-15 11:28:21 -0300

    .

---
+++ script.js
@@ -0,0 +1,44 @@
+const caixaPrincipal =document.querySelector(".caixa-principal");
+const caixaPerguntas =document.querySelector(".caixa-perguntas");
+const caixaPerguntas =document.querySelector(".caixa-alternativas");
+const caixaPerguntas =document.querySelector(".caixa-resultado");
+const caixaPerguntas =document.querySelector(".texto-resultado");
+
+const perguntas =[
+{
+        enunciado:"Pergunta 2",
+alternativas: [
+    "Alternativa 1";
+    "Alternativa 2"
+]
+},    
+
+{
+enunciado:"Pergunta 2",
+alternativas: [
+    "Alternativa 1";
+    "Alternativa 2"
+]
+},    
+{
+enunciado:"Pergunta 3",
+alternativas: [
+    "Alternativa 1";
+    "Alternativa 2"
+]
+},
+{
+enunciado:"Pergunta 4 ",
+alternativas: [
+    "Alternativa 1";
+    "Alternativa 2"
+]
+},    
+{
+enunciado:"Pergunta 5",
+alternativas: [
+    "Alternativa 1";
+    "Alternativa 2"
+]
+}
+];
